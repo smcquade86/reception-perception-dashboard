bd3479ee0e6b9e62dd2f8430575e6ba3a3caa225
smcquade86 <sean.r.mcquade@gmail.com>
2024-09-25 16:55:59 -0500

    Removed referer requirement for now

--- src/pages/api/googleSheets.ts
+++ src/pages/api/googleSheets.ts
@@ -87,25 +87,10 @@ async function fetchGoogleSheetData(): Promise<{ players: RowData[], similarPlay
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const referer = req.headers.referer;
-  const origin = req.headers.origin;
-  const isLocal = process.env.NODE_ENV === 'development';
-
-  console.log('Referer:', referer);
-  console.log('Origin:', origin);
-  console.log('Is Local:', isLocal);
-
-  // Check if the request is coming from the allowed domain or if it's a local development environment
-  if (!isLocal && (!referer?.startsWith('https://receptionperception.com') && !origin?.startsWith('https://receptionperception.com'))) {
-    console.log('Forbidden request');
-    return res.status(403).json({ error: 'Forbidden' });
-  }
-
   try {
     const data = await fetchGoogleSheetData();
     res.status(200).json(data);
-  } catch (error) {
-    console.error('Error in handler:', error);
+  } catch {
     res.status(500).json({ error: 'Failed to fetch Google Sheets data' });
   }
 }
