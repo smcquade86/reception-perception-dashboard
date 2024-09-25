0e242145dc7623def28e7af19cab0c52e78001b1
smcquade86 <sean.r.mcquade@gmail.com>
2024-09-25 13:33:22 -0500

    Adjusted path on next.confit.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
-    trailingSlash: true,
-    // Add any other necessary configurations here
-  };
-  
-  export default nextConfig;
+  trailingSlash: true,
+  basePath: isProd ? '/reception-perception-dashboard' : '',
+  assetPrefix: isProd ? '/reception-perception-dashboard/' : '',
+  // Add any other necessary configurations here
+};
+
+export default nextConfig;
