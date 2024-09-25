d2b1c12af9ddfaf8948d2f3252b7fd3aba872f79
smcquade86 <sean.r.mcquade@gmail.com>
2024-09-25 15:23:50 -0500

    Updated files for vercel and deleted vercel.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
   trailingSlash: true,
-  basePath: isProd ? '/reception-perception-dashboard' : '',
-  assetPrefix: isProd ? '/reception-perception-dashboard/' : ''
+  // Remove basePath and assetPrefix if not needed
 };
 
 export default nextConfig;
