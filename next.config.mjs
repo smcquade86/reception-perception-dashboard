e90add8d3f9dd4c926e8c36a9546b0425a333504
smcquade86 <sean.r.mcquade@gmail.com>
2024-09-25 14:56:02 -0500

    Switch deployment to Vercel 2

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,7 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   trailingSlash: true,
   basePath: isProd ? '/reception-perception-dashboard' : '',
-  assetPrefix: isProd ? '/reception-perception-dashboard/' : '',
-  // Remove output: 'export' as it is not needed
+  assetPrefix: isProd ? '/reception-perception-dashboard/' : ''
 };
 
 export default nextConfig;
